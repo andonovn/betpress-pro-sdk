e5f04cf9ebf8bd08a16e2ee4d6764bc2fe219918
Nikolay Andonov <nikolay.andonov@windowslive.com>
2023-06-10 02:33:02 +0300

    GetLeaderboardStandings

---
+++ src/GetLeaderboardStandings.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Andonovn\BetpressProSdk;
+
+use Andonovn\BetpressProSdk\ApiException;
+use GuzzleHttp\Exception\GuzzleException;
+
+class GetLeaderboardStandings extends Call
+{
+    public function handle($id)
+    {
+        try {
+            return $this->asArray(
+                $this->http->get('leaderboards/'.$id.'/standings')->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new ApiException('API call to leaderboards has failed. Please try again later! Message: ' . $e->getMessage());
+        }
+    }
+}
