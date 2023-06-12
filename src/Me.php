2bcf5e549346b8cde68bc16f7ba58646540a8f8d
Nikolay Andonov <nikolay.andonov@windowslive.com>
2023-06-12 05:40:58 +0300

    Me.php to return array

--- src/Me.php
+++ src/Me.php
@@ -10,7 +10,9 @@ class Me extends Call
     public function handle()
     {
         try {
-            return $this->http->get('me')->getBody();
+            return $this->asArray(
+                $this->http->get('me')->getBody()
+            );
         } catch (GuzzleException $e) {
             throw new ApiException('API call to the "me" endpoint has failed. Please try again later! Message: '.$e->getMessage());
         }
