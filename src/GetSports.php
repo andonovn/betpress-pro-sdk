ed1f8bcbf549bc674d8d4ac40b7e6b011f0d3c30
Nikolay Andonov <nikolay.andonov@windowslive.com>
2023-06-18 02:22:41 +0300

    GetSports. GetGames filtered by sport

--- src/GetSports.php
+++ src/GetSports.php
@@ -5,20 +5,20 @@ namespace Andonovn\BetpressProSdk;
 use Andonovn\BetpressProSdk\ApiException;
 use GuzzleHttp\Exception\GuzzleException;
 
-class GetGames extends Call
+class GetSports extends Call
 {
-    public function handle($sport)
+    public function handle()
     {
         try {
             return $this->asArray(
-                $this->http->get('games?sport='.$sport)->getBody()
+                $this->http->get('sports')->getBody()
             );
         } catch (GuzzleException $e) {
             if (method_exists($e, 'getResponse') && $e->getResponse()->getStatusCode() === 402) {
                 throw new ExpiredSubscriptionException($e->getMessage());
             }
 
-            throw new ApiException('API call to games has failed. Please try again later! Message: ' . $e->getMessage());
+            throw new ApiException('API call to sports has failed. Please try again later! Message: ' . $e->getMessage());
         }
     }
 }
